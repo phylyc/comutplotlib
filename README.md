b4001df7708c565bf26cc3d592c5384a905704e9
BritneySZ <britneyszhang@gmail.com>
2023-07-19 11:47:40 -0400

    add "validate_flags" argument & allow multiple inputs to flags

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # comut_plot
+
+Project Overview:
+
+Installation Instructions: see install.sh and setup.cfg
+
+Usage Guide:
+
+Features:
+
+Documentation:
+
+Examples:
+
+Contribution Guidelines:
+
+Project Structure:
+
+Dependencies:
+
+Known Issues or Limitations:
+
+Contact Information:
--- src/comut_argparse.py
+++ src/comut_argparse.py
@@ -10,18 +10,26 @@ def parse_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.usage = 'use to...'
+    def validate_flags(args):
+        if args.maf is None and args.gistic is None:
+         return args
 
     parser.add_argument("-o", "--output", type=str, required=True, help="Path to the output file.")
-    parser.add_argument("--maf", type=str, required=False, default=None, help="Path to the MAF input file.")
-    parser.add_argument("--sif", type=str, required=False, default=None, help="Path to the SIF input file.")
-    parser.add_argument("--gistic", type=str, required=False, default=None, help="Path to the MAF input file.")
-    parser.add_argument("--meta_data_rows_per_sample", type=str, required=False, default="Sample Type,Material,Contamination,Tumor Purity,Platform", help="Comma separated list of SIF columns to plot per sample.")
+    parser.add_argument("--maf", type=str, required=False, default=None, action='append', help="Path to the MAF input file.")
+    parser.add_argument("--sif", type=str, required=False, default=None, action='append', help="Path to the SIF input file.")
+    parser.add_argument("--gistic", type=str, required=False, default=None, action='append', help="Path to the MAF input file.")
+
     parser.add_argument("--by", type=str, choices=["Sample", "Patient"], required=False, default="Patient")
     parser.add_argument("--label_columns", type=str, required=False, default=False, help="labels at the bottom of plot")
     parser.add_argument("--column_order", type=str, required=False, default=None, help="order of the columns on plot")
     parser.add_argument("--index_order", type=str, required=False, default=None, help="order of the index on plot")
+
     parser.add_argument("--meta", type=str, required=False, default=None, help="Path to the meta input file.")
     parser.add_argument("--meta_data_rows", type=str, required=False, default="Sample Type,Material,Contamination,Tumor Purity,Platform,has_matched_N,Sex,Histology", help="Comma separated list of SIF columns to plot.")
+    parser.add_argument("--meta_data_rows_per_sample", type=str, required=False,
+                        default="Sample Type,Material,Contamination,Tumor Purity,Platform",
+                        help="Comma separated list of SIF columns to plot per sample.")
+
     parser.add_argument("--genes", type=str, required=False, default=None, help="Comma separated list of genes.")
     parser.add_argument("--snv_interesting_genes", type=str, required=False, default=None,
                         help="Comma separated list of snv genes.")
@@ -57,6 +65,9 @@ def parse_args():
         args.snv_interesting_genes = args.snv_interesting_genes.split(",")
     if args.cnv_interesting_genes is not None:
         args.cnv_interesting_genes = args.cnv_interesting_genes.split(",")
+
+    #Validate the flags using the custom function
+    args = validate_flags(args)
 
     args.meta_data_rows = args.meta_data_rows.split(",")
     args.meta_data_rows_per_sample = args.meta_data_rows_per_sample.split(",")
