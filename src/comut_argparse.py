6eb2de9af3ca7c4f4e25f8e93332ff8548b02141
BritneySZ <britneyszhang@gmail.com>
2023-07-31 10:28:36 -0400

    changed usage guide for README.md

--- src/comut_argparse.py
+++ src/comut_argparse.py
@@ -10,11 +10,9 @@ def parse_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.usage = 'use to...'
-
     def validate_flags(args):
         if args.maf is None and args.gistic is None:
-            raise ValueError("Either --maf or --gistic must be specified.")
-        return args
+         return args
 
     parser.add_argument("-o", "--output", type=str, required=True, help="Path to the output file.")
     parser.add_argument("--maf", type=str, required=False, default=None, action='append', help="Path to the MAF input file.")
@@ -22,7 +20,7 @@ def parse_args():
     parser.add_argument("--gistic", type=str, required=False, default=None, action='append', help="Path to the MAF input file.")
 
     parser.add_argument("--by", type=str, choices=["Sample", "Patient"], required=False, default="Patient")
-    parser.add_argument("--label_columns", type=bool, required=False, default=True, help="labels at the bottom of plot")
+    parser.add_argument("--label_columns", type=str, required=False, default=False, help="labels at the bottom of plot")
     parser.add_argument("--column_order", type=str, required=False, default=None, help="order of the columns on plot")
     parser.add_argument("--index_order", type=str, required=False, default=None, help="order of the index on plot")
 
@@ -39,7 +37,7 @@ def parse_args():
                         help="Comma separated list of cnv genes")
     parser.add_argument("--interesting_gene_comut_threshold_percent", type=float, required=False, default=None,
                         help="threshold of significance for interesting genes")
-    parser.add_argument("--ground_truth_genes", type=str, required=False, default=None, action="append",
+    parser.add_argument("--ground_truth_genes", type=str, required=False, default=None,
                         help="dictionary of genes to compare to")
     parser.add_argument("--low_amp_threshold", type=int, required=False, default=1, help="threshold for low amplification")
     parser.add_argument("--high_amp_threshold", type=int, required=False, default=2, help="threshold for high amplification")
@@ -48,7 +46,7 @@ def parse_args():
     parser.add_argument("--total_prevalence_threshold", type=float, required=False, default=None,
                         help="threshold for total prevalence")
     parser.add_argument("--model_names", type=str, required=False, default="SNV burden model,CNV burden model", help="names of models.")
-    parser.add_argument("--panels_to_plot", type=str, required=False, default="tmb,recurrence,prevalence,total prevalence,total prevalence overall,cytoband,gene names,model annotation,comutation,snv legend,cnv legend,model annotation legend,meta data,meta data legend", help="parts to be plotted")
+    parser.add_argument("--parts_to_plot", type=str, required=False, default="mutation burden,mutation burden legend,recurrence,prevalence,total prevalence,cytoband,comutations,comutations legend,meta data,meta data legend", help="parts to be plotted")
     parser.add_argument("--max_xfigsize", type=int, required=False, default=None, help="maximum x figure size")
     parser.add_argument("--yfigsize", type=int, required=False, default=None, help ="y figure size")
     parser.add_argument("--sub_folders", type=str, required=False, default=None, help="sub folders")
@@ -67,16 +65,13 @@ def parse_args():
         args.snv_interesting_genes = args.snv_interesting_genes.split(",")
     if args.cnv_interesting_genes is not None:
         args.cnv_interesting_genes = args.cnv_interesting_genes.split(",")
-    if args.ground_truth_genes is not None:
-        args.ground_truth_genes = {
-            k: v.split(",") for k, v in [x.split(":") for x in args.ground_truth_genes]
-        }
 
+    #Validate the flags using the custom function
     args = validate_flags(args)
 
     args.meta_data_rows = args.meta_data_rows.split(",")
     args.meta_data_rows_per_sample = args.meta_data_rows_per_sample.split(",")
     args.model_names = args.model_names.split(",")
-    args.panels_to_plot = args.panels_to_plot.split(",")
+    args.parts_to_plot = args.parts_to_plot.split(",")
 
     return args
