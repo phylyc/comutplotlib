45a2352b0dd2baa3220f4c18030cef7911ac1ab6
phylyc <0d26ae7a@opayq.com>
2024-04-23 18:37:58 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 # This script is to be executed within the folder.
 # First: download and install e.g. miniconda: https://docs.conda.io/projects/miniconda/en/latest/
 # Then: run this script.
-conda create -n comutplotlib -y python=3.11
-conda activate comutplotlib
+#conda create -n comutplotlib -y python=3.11
+#conda activate comutplotlib
+
+# If you want to install the package in an existing environment, just run this line:
 python -m pip install --editable .
