b204f4616aeef18745df61541e539b93b9094493
phylyc <0d26ae7a@opayq.com>
2026-02-02 12:59:20 -0500

    update segmentation handling

--- comutplotlib/seg.py
+++ comutplotlib/seg.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+import gzip
 import numpy as np
 import os
 import pandas as pd
@@ -12,10 +13,7 @@ def join_segs(segs: list["SEG"], verbose=False):
     elif len(segs) == 1:
         return segs[0].copy()
     else:
-        seg = segs[0]
-        for other in tqdm(segs[1:], desc="Joining SEGs", total=len(segs), initial=1, disable=not verbose):
-            seg = seg.join(other=other)
-        return seg
+        return SEG(data=pd.concat([seg.data for seg in segs], ignore_index=True))
 
 
 class SEG(object):
@@ -32,21 +30,24 @@ class SEG(object):
     _gene_name = "gene_name"
 
     @classmethod
-    def from_file(cls, path_to_file: str, encoding: str = "utf8"):
+    def from_file(cls, path_to_file: str, encoding: str = "utf8", columns: list[str] = None, log_tCR_base: float = 2):
         if not os.path.exists(path_to_file):
             raise FileNotFoundError(
                 f"No SEG instance created. No such file or directory: '{path_to_file}'"
             )
-        with open(path_to_file, encoding=encoding) as f:
-            data = pd.read_csv(filepath_or_buffer=f, sep="\t", engine="c", comment="#")
+        open_func = gzip.open if path_to_file.endswith(".gz") else open
+        with open_func(path_to_file, mode="rt", encoding=encoding) as f:
+            data = pd.read_csv(filepath_or_buffer=f, sep="\t", engine="c", comment="#", header=0, names=columns)
+        data[cls._log2_tCR] *= np.log(log_tCR_base) / np.log(2)
         return SEG(data=data)
 
-    def __init__(self, data=None):
+    def __init__(self, data=None, add_gene_names=True):
         self.data = (
             data if data is not None
             else pd.DataFrame(None, columns=pd.Index([self._sample, self._contig, self._start, self._end, self._log2_tCR, self._gene_name]))
         )
-        self.add_gene_name()
+        if add_gene_names:
+            self.add_gene_name()
 
     def add_gene_name(self, verbose=False):
         data_is_incomplete = (
@@ -66,7 +67,7 @@ class SEG(object):
                 ):
                     _gene_names[index] = genome.gene_names_at_locus(
                         contig=segment.get(self._contig),
-                        position=segment.get(self._end),
+                        position=segment.get(self._start),
                         end=segment.get(self._end),
                     )
                 return pd.Series(_gene_names)
@@ -74,14 +75,21 @@ class SEG(object):
             self.data[self._gene_name] = get_gene_names(df=self.data)
 
     def get_df(self) -> pd.DataFrame:
-        return (
-            self.data[[self._sample, self._log2_tCR, self._gene_name]]
-            .explode(self._gene_name)
-            .groupby([self._sample, self._gene_name])
-            .agg({self._log2_tCR: lambda x: x.values[np.argmax(np.abs(x.values))]})
-            [self._log2_tCR]
-            .unstack(self._sample)
+        s, v, g = self._sample, self._log2_tCR, self._gene_name
+        df = self.data[[s, v, g]].explode(g, ignore_index=True)
+
+        # use categoricals for the group keys to save memory
+        if df[g].dtype != "category":
+            df[g] = pd.Categorical(df[g])
+        if df[s].dtype != "category":
+            df[s] = pd.Categorical(df[s])
+
+        idx = (
+            pd.Series(df[v].abs().to_numpy(), index=df.index)
+            .groupby([df[s], df[g]], observed=True, sort=False)
+            .idxmax()
         )
+        return df.loc[idx, [g, s, v]].pivot(index=g, columns=s, values=v)
 
     def select_genes(self, genes: list[str], inplace=True) -> "SEG":
         gene_col = self.data[self._gene_name].apply(lambda _genes: [g in genes for g in _genes])
